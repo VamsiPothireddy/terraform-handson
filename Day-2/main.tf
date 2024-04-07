2155b2df5d16dfd1b0259cf99f7378b90baaab4a
vamsipothireddy <vamsikrishnapothireddy@gmail.com>
2024-04-07 09:28:47 -0400

    Isolated provider.tf file

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-    region = "us-east-2"  # Set your desired AWS region
-}
 
 resource "aws_instance" "example" {
     ami           = var.ami_type  # Specify an appropriate AMI ID1
---
+++ Day-2/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+    alias  = "default"
+    region = "us-east-2"  # Set your desired AWS region
+}
